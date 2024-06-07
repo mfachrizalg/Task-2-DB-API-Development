1306cddade797e25f1f557360f0055e3a86a6235
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-06-07 22:37:41 +0700

    fix : wishlist

--- api/controllers/wishlistController.js
+++ api/controllers/wishlistController.js
@@ -13,6 +13,7 @@ const getWishlist = (req, res) => {
     const userid = req.params.id;
     pool.query(queries.getWishlistbyUserID, [userid], (error, results) => {
         if (error) res.status(400).json({'message' :`${error}`});
+        if (results.rows.length == 0) res.status(404).json({'message' : 'No books in wishlist'});
         res.status(200).json(results.rows);
     });
 }
